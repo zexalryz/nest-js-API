b7d3c1f7c6bf8cf44e621b6bedfed4cb5e02196e
[C <rizk@mekansm.24q5bdjzzz3ejjppk3535mfjoe.px.internal.cloudapp.net>
2022-06-13 03:48:07 +0000

    update cuy

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+    console.log(config.get('DATABASE_URL'))
+  }
+}
